161352b7eb4cf9bcbcc26df42d988a97f79cf411
takketan07-lgtm <takketan07@gmail.com>
2026-06-15 01:43:56 +0900

    Update Google Maps link formatting in view.php

--- view.php
+++ view.php
@@ -5,16 +5,16 @@ if (file_exists("location.txt")) {
 
     $text = file_get_contents("location.txt");
 
-    // Googleマップリンクを自動でクリック可能にする
+    // Googleマップリンクをクリック可能にする
     $text = preg_replace(
         '/(https:\/\/maps\.google\.com\/\?q=[0-9\.\-]+,[0-9\.\-]+)/',
-        '<a href="$1" target="_blank">$1（地図を開く）</a>',
+        '<a href="$1" target="_blank">📍 地図を開く</a>',
         $text
     );
 
-    echo "<pre style='white-space:pre-wrap;'>";
+    echo "<div style='white-space:pre-line; font-family:sans-serif;'>";
     echo $text;
-    echo "</pre>";
+    echo "</div>";
 
 } else {
     echo "まだデータがありません。";
